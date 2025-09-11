ae781a5792a8bbfaa3c8ff9a67a4bba0d9a3d7b8
sai11pavan <96167294+sai11pavan@users.noreply.github.com>
2025-09-11 15:43:19 +0200

    Add files via upload

---
+++ robot05.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+import base64
+import json
+from openai import OpenAI
+import ollama
+def encode_image(path):
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read()).decode("utf-8")
+    
+class Task05(BaseModel):
+    color: str
+    shape: str
+
+
+image="C:\\Users\\Admin\\Downloads\\niryo\\AISummerSchool\\outputGREENWORKSPACE.jpg"
+
+system_prompt = (
+            "Imagine Yourself as a vision assistant for robotics. "
+            "Identify the shape of the objects "
+            "and its color (red, green, blue). "
+            "Output only valid JSON that matches the following schema: "
+            '{"shape":"<shape of the object>","color":"<color of the object>"} '
+            "Do NOT include any extra text or explanation."
+        )
+
+
+def openaillm(image):
+    client= OpenAI(api_key="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")
+
+    encoded_image = encode_image(image)
+
+    
+    response = client.chat.completions.parse(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content" : [
+                {"type": "text", "text": "Find the shape and color in this image."},
+                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{encoded_image}"}}
+            ]}
+        ]
+    )
+    raw = response.choices[0].message.content
+    parsed = Task05(**json.loads(raw))
+    print("Parsed result:", parsed.shape, parsed.color)
+
+
+
+openaillm(image)
+
